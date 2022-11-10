b0ff3fa2d47f8b4b545c4be6dc7bc929b68f4e4d
Seung-Woo Han <117332944+1AfterWon@users.noreply.github.com>
2022-11-10 22:56:28 +0900

    [level 1] Title: 약수의 합, Time: 0.07 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12928. 약수의 합/약수의 합.java
+++ 프로그래머스/lv1/12928. 약수의 합/약수의 합.java
@@ -1,15 +1,12 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        int[] arr=new int[100];
-        int cnt=0;
-        for(int i=1;i<=n;i++){
+        for(int i=1;i<=n/2;i++){ // n/2까지만 돌려도 약수는 다 구할 수 있다.
             if(n%i==0){
-                arr[cnt]=i;
-                cnt++;
                 answer+=i;
             }
         }
+        answer+=n;  //마지막에 n만 추가해주면 모든 약수를 더할 수 있다.
         return answer;
     }
 }
