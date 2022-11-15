663dff72a2daf47e4f5680495f2277b91d93fbe3
Seung-Woo Han <117332944+1AfterWon@users.noreply.github.com>
2022-11-15 09:46:07 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 11.64 ms, Memory: 79.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
@@ -0,0 +1,26 @@
+class Solution {
+    public long solution(long n) {
+        long answer = 0;
+        
+        //n의 자릿수만큼 배열 생성 
+        int[] arr=new int[(int)(Math.log10(n)+1)]; 
+        for(int i=0;i<arr.length;i++){
+            arr[i]=(int)(n%10);
+            n/=10;
+        }
+        for(int i=0;i<arr.length-1;i++){
+            for(int j=0;j<arr.length-1-i;j++){ //버블정렬 이용해서 내림차순으로 정렬
+                if(arr[j]<arr[j+1]){
+                    int tmp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=tmp;
+                }
+            }
+        }
+         String s=""; //배열의 값을 받을 문자열 생성
+         for(int a:arr){s+=a; }//문자열에 배열의 데이터 하나씩 넣어서 문자열로 변경
+         answer=Long.parseLong(s); //문자열을 long 값으로 변경
+        
+        return answer;
+    }
+}
