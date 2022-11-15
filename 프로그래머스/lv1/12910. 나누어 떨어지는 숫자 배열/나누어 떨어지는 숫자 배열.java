be8a8187e2c2fc58db10fa0e79f8c063aade4ccd
Seung-Woo Han <117332944+1AfterWon@users.noreply.github.com>
2022-11-15 23:08:46 +0900

    [level 1] Title: 나누어 떨어지는 숫자 배열, Time: 0.50 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/lv1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.java
+++ 프로그래머스/lv1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.java
@@ -8,13 +8,13 @@ class Solution {
         
         //arr의 값을 하나씩 받아서 나눠지면 cnt를 하나씩 늘림
         for(int a:arr){if(a%divisor==0)cnt++;} 
-        //나눠지는 수가 하나도 없으면 길이 1로 선언해서 -1을 넣음
-        if(cnt==0){answer=new int[1];answer[0]=-1;}
+        //나눠지는 수가 하나도 없으면 {-1}의 배열로! 
+        if(cnt==0){answer=new int[]{-1};}
         //나눠지는 수가 있으면 그 길이만큼 배열 선언 후
         else if(cnt>0){
             answer=new int[cnt];
             int i=0;
-            //배열에 나눠지는 값들 추가
+            //배열에 나눠지는 값들 추가!
             for(int a:arr){
                 if(a%divisor==0){answer[i]=a;i++;}
             }
