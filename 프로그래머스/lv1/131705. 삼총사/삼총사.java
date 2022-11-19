ace9ae7683eeb7e6af5f8be2a399666a98a64236
Seung-Woo Han <117332944+1AfterWon@users.noreply.github.com>
2022-11-20 00:53:26 +0900

    [level 1] Title: 삼총사, Time: 0.03 ms, Memory: 74.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int[] number) {
+        int answer = 0;
+        int cnt=0;
+        //i j k 3가지 수가 있을 때, 서로 중복되는 경우가 없게 i, j=i+1, k=j+1로 설정하여 합이 0 인지 확인
+        for(int i=0;i<number.length;i++)
+            for(int j=i+1;j<number.length;j++)
+                for(int k=j+1;k<number.length;k++)
+                    if(number[i]+number[j]+number[k]==0)cnt++; //합이 0이면 cnt 증가
+                
+        answer=cnt;    //answer에 cnt 넣기!
+        return answer;
+    }
+}
