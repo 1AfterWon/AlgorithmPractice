86ab6aaa306b8b48e8cd7279b130393808346d3b
Seung-Woo Han <117332944+1AfterWon@users.noreply.github.com>
2022-11-20 00:54:47 +0900

    Update 삼총사.java
    다른사람 풀이 보고 주석 추가

--- 프로그래머스/lv1/131705. 삼총사/삼총사.java
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.java
@@ -7,6 +7,8 @@ class Solution {
             for(int j=i+1;j<number.length;j++)
                 for(int k=j+1;k<number.length;k++)
                     if(number[i]+number[j]+number[k]==0)cnt++; //합이 0이면 cnt 증가
+        
+        //다른사람 풀이를 보니 cnt를 굳이 선언하지 않고 바로 answer++로 해도 되는거였다!
                 
         answer=cnt;    //answer에 cnt 넣기!
         return answer;
